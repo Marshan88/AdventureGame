99ee1d4ab157c74cf94b5f9914a473b570240185
Marshan88 <92783803+Marshan88@users.noreply.github.com>
2022-08-26 10:45:04 +0200

    Add project files.

---
+++ Adventure game/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure_game
+{
+    internal static class Game
+    {
+        public static void StartGame()
+        {
+            string CharacterName = "";
+            Console.WriteLine("Game title");
+            Console.WriteLine("Name your character:");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Welcome to Runeterra, " + CharacterName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Adventure game/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Adventure_game
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Game.StartGame();
+            Console.ReadKey();
+        }
+    }
+}
